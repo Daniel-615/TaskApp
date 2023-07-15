b67808979825f6702c8adaa4cff8997cad391562
Daniel-615 <suyanangel615@gmail.com>
2023-07-15 12:11:47 -0600

    db

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -66,9 +66,24 @@ WSGI_APPLICATION = "djangocrud.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASE_URL = os.environ.get('DATABASE_URL')
-DATABASES={
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
-}
+try:
+    # Intenta cargar la configuración desde la URL de conexión
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+    }
+except Exception:
+    # Si ocurre una excepción (por ejemplo, si DATABASE_URL no está configurado correctamente),
+    # utiliza una configuración de base de datos alternativa
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('DATABASE_NAME'),
+            'USER': os.environ.get('DATABASE_USER'),
+            'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+            'HOST': os.environ.get('DATABASE_HOSTNAME'),
+            'PORT': os.environ.get('DATABASE_PORT'),
+        }
+    }
 print(DATABASE_URL)
 
 # Password validation
